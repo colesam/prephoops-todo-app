7b36c8282dcc581ffa1c4391ae3dd1a696323aa7
Samuel Cole <cole.samuel21@gmail.com>
2019-04-06 14:17:31 -0500

    Begin oauth login route

--- backend/app/Http/Controllers/Auth/LoginController.php
+++ backend/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Request as RequestFacade;
+use Route;
+use DB;
 
 class LoginController extends Controller
 {
@@ -35,5 +39,41 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $oauthPasswordClient = DB::connection()->table('oauth_clients')
+            ->select([
+                'id',
+                'secret'
+            ])
+            ->where('name', 'Laravel Password Grant Client')
+            ->first();
+
+        if (is_null($oauthPasswordClient))
+        {
+            return response()->json('Something is wrong with the server configuration.', 500);
+        }
+
+        try
+        {
+            // Add parameters to request to abstract oauth details from client
+            $new_request = RequestFacade::create('/oauth/token', 'POST', [
+                'grant_type' => 'password',
+                'client_id' => $oauthPasswordClient->id,
+                'client_secret' => $oauthPasswordClient->secret,
+                'username' => $request->username,
+                'password' => $request->password,
+            ]);
+            RequestFacade::replace($new_request->input());
+
+            /** @noinspection PhpParamsInspection */
+            return Route::dispatch($new_request);
+        }
+        catch(\Exception $e)
+        {
+            return response()->json('Something went wrong with the server.', 500);
+        }
     }
 }
