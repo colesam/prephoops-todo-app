2544414f9e8f2837595e887709331236bcff5d7d
Samuel Cole <cole.samuel21@gmail.com>
2019-04-07 08:51:51 -0500

    Add eslint and prettier tooling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import React, {Component} from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 const TestComponent = () => {
-  return (
-    <h1>HelloWorld</h1>
-  );
+  return <h1>HelloWorld</h1>;
 };
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Route path="/test" component={TestComponent}/>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Route path="/test" component={TestComponent} />
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
