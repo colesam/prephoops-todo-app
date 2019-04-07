0d7eafce729838530342814b53c8b81721354571
Samuel Cole <cole.samuel21@gmail.com>
2019-04-07 13:29:32 -0500

    Add registration view

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './views/Login';
+import Register from './views/Register';
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Route path="/register" component={Register} />
         <Route path="/" component={Login} />
       </BrowserRouter>
     </div>
---
+++ frontend/src/js/views/Register.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import axios from 'axios';
+import BaseLayout from './BaseLayout';
+import TextField from '../components/TextField';
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      passwordConfirm: '',
+      usernameError: null,
+      passwordError: null,
+      passwordConfirmError: null
+    };
+  }
+
+  onUsernameChange(event) {
+    this.setState({ username: event.target.value });
+  }
+
+  onPasswordChange(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  onPasswordConfirmChange(event) {
+    this.setState({ passwordConfirm: event.target.value });
+  }
+
+  onRegisterClick() {
+    if (
+      this.validateUsername() &&
+      this.validatePassword() &&
+      this.validatePasswordConfirm()
+    ) {
+      // submit request
+      axios
+        .post('http://localhost:9000/api/register', {
+          username: this.state.username,
+          password: this.state.password,
+          password_confirmation: this.state.passwordConfirm
+        })
+        .then(response => {
+          console.log('=== Response ===');
+          console.log(response);
+        })
+        .catch(error => {
+          console.log('=== Error ===');
+          console.log(error);
+        });
+    }
+  }
+
+  validateUsername() {
+    const username = this.state.username;
+    const emailRegex = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (username == null || username.length < 1) {
+      this.setState({ usernameError: 'Please enter a username.' });
+      return false;
+    }
+
+    if (!username.match(emailRegex)) {
+      this.setState({ usernameError: 'Please enter a valid email address.' });
+      return false;
+    }
+
+    this.setState({ usernameError: null });
+    return true;
+  }
+
+  validatePassword() {
+    const password = this.state.password;
+
+    if (password == null || password.length < 1) {
+      this.setState({ passwordError: 'Please enter a password.' });
+      return false;
+    }
+
+    this.setState({ passwordError: null });
+    return true;
+  }
+
+  validatePasswordConfirm() {
+    const passwordConfirm = this.state.passwordConfirm;
+
+    if (passwordConfirm !== this.state.password) {
+      this.setState({ passwordConfirmError: 'Password does not match.' });
+      return false;
+    }
+
+    this.setState({ passwordConfirmError: null });
+    return true;
+  }
+
+  render() {
+    return (
+      <BaseLayout>
+        <div className="col-6">
+          <div className="card box-shadow">
+            <div className="card-body">
+              <div className="card-title mb-4">
+                <h1 className="h3">Register</h1>
+              </div>
+              <div className="card-text">
+                <TextField
+                  label="Email"
+                  type="email"
+                  id="username"
+                  helpMessage="This will be your username."
+                  errorMessage={this.state.usernameError}
+                  onChange={event => this.onUsernameChange(event)}
+                  onBlur={() => this.validateUsername()}
+                />
+                <TextField
+                  label="Password"
+                  type="password"
+                  id="password"
+                  isPassword={true}
+                  errorMessage={this.state.passwordError}
+                  onChange={event => this.onPasswordChange(event)}
+                  onBlur={() => this.validatePassword()}
+                />
+                <TextField
+                  label="Confirm Password"
+                  type="password"
+                  id="password-confirm"
+                  isPassword={true}
+                  errorMessage={this.state.passwordConfirmError}
+                  onChange={event => this.onPasswordConfirmChange(event)}
+                  onBlur={() => this.validatePasswordConfirm()}
+                />
+                <div className="row">
+                  <div className="col-12 col-md-6">
+                    <button
+                      className="btn btn-primary form-control"
+                      onClick={() => this.onRegisterClick()}
+                    >
+                      Register
+                    </button>
+                  </div>
+                  <div className="col-12 col-md-6">
+                    <button className="btn btn-link form-control">Back</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </BaseLayout>
+    );
+  }
+}
+
+export default Register;
