2ba3c30e1a8994a0733e2ab81dbfbc42d32b3124
Samuel Cole <cole.samuel21@gmail.com>
2019-04-07 18:41:08 -0500

    Add in Todo table migration and seeder

---
+++ backend/database/migrations/2019_04_07_232635_create_todo_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id');
+            $table->string('name', 100);
+            $table->boolean('is_checked');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo');
+    }
+}
---
+++ backend/database/seeds/TodoTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TodoTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $todos = [
+            [
+                'user_id' => 1, // cole.samuel21@gmail.com
+                'name' => 'Walk my dog',
+                'is_checked' => false,
+                'created_at' => Carbon\Carbon::now(),
+                'updated_at' => Carbon\Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'name' => 'Take out the trash',
+                'is_checked' => false,
+                'created_at' => Carbon\Carbon::now(),
+                'updated_at' => Carbon\Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'name' => 'Finish todo app',
+                'is_checked' => true,
+                'created_at' => Carbon\Carbon::now(),
+                'updated_at' => Carbon\Carbon::now()
+            ],
+        ];
+
+        foreach ($todos as $todo)
+        {
+            DB::table('todo')->insert($todo);
+        }
+    }
+}
