7463e672a5ddb24d7bae8248bb57e145ef86de93
Samuel Cole <cole.samuel21@gmail.com>
2019-04-07 08:31:40 -0500

    Add in basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+
+const TestComponent = () => {
+  return (
+    <h1>HelloWorld</h1>
+  );
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <BrowserRouter>
+          <Route path="/test" component={TestComponent}/>
+        </BrowserRouter>
       </div>
     );
   }
