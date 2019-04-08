807e6fa174fd90930ce843ab30f98050e2b18f78
Samuel Cole <cole.samuel21@gmail.com>
2019-04-07 19:03:37 -0500

    Begin find route of todo controller

--- backend/app/Http/Controllers/TodoController.php
+++ backend/app/Http/Controllers/TodoController.php
@@ -11,4 +11,16 @@ class TodoController extends Controller
     {
         return response()->json(Auth::user()->todos, 200);
     }
+
+    public function find(Request $request)
+    {
+        $todo = Auth::user()->todos()->where('id', $request->todo_id);
+
+        if (is_null($todo))
+        {
+            return response()->json('Todo not found.', 400);
+        }
+
+        return response()->json($todo, 200);
+    }
 }
